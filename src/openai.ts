6c6ecd22603f59833b029fe2441b139bc9b1033c
Arturo <arturo10agudelo@gmail.com>
2025-09-02 22:21:47 -0500

    change from openAI to gemini

--- src/openai.ts
+++ src/openai.ts
@@ -1,7 +1,22 @@
-import OpenAI from "openai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-export const openai = new OpenAI({
-    apikey: process.env.OPENAI_API_KEY,
-});
+const geminiApiKey = process.env.GEMINI_API_KEY;
 
-console.log(process.env.OPENAI_API_KEY)
+if (!geminiApiKey) {
+    throw new Error("GEMINI_API_KEY no está definida. Por favor, asegúrate de que tu variable de entorno esté configurada correctamente.");
+}
+
+export const genAI = new GoogleGenerativeAI(geminiApiKey);
+
+async function prueba() {
+    const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
+
+    const prompt = "Hola, puedes resumirme que es bun?";
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+}
+
+prueba();
